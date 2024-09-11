2715d0a6cbf334578b197a1a432343e3657b1296
Zain Ul Abideen <141809779+MalikZain1014@users.noreply.github.com>
2024-09-11 19:18:50 +0500

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -4,7 +4,7 @@ export const contactsData = {
   address: "Mandi Mor Pākpattan, Pakistan - 57400 ",
   github: "https://github.com/MalikZain1014/",
   facebook: "https://www.facebook.com/malikzain.zain.5076/",
-  linkedIn: "https://www.linkedin.com/in/malik-zain-a60a172b8",
+  linkedIn: "https://www.linkedin.com/in/zain-ul-abideen18135b9296/",
   twitter: "https://twitter.com/MalikZa03337218",
   stackOverflow: "https://stackoverflow.com/users/23572334/malik-zain/",
   devUsername: "Malik_Z",
