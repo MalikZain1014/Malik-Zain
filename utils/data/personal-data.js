3fa51e374e2d8380e8e786ac4d1246fa549fd00b
Zain Ul Abideen <141809779+MalikZain1014@users.noreply.github.com>
2025-12-20 00:07:39 +0500

    .

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -6,7 +6,7 @@ export const personalData = {
     "My name is Zain Ul Abideen. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: "malikzain1014@gmail.com",
   phone: "+923090201802",
-  address: "Mandi Mor Pākpattan, Pakistan - 57400 ",
+  address: "House 1, C 2 Township, Lahore",
   github: "https://github.com/MalikZain1014/",
   facebook: "https://www.facebook.com/malikzain.zain.5076/",
   linkedIn: "https://www.linkedin.com/in/zain-ul-abideen18135b9296/",
